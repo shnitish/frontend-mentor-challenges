e358b3363585080c53c0a3240ea1c9b0a32426d5
Nitish Sharma <nitishkumar81299@gmail.com>
2023-04-29 12:11:22 +0530

    static data for the page

---
+++ room-homepage/src/static/data.ts
@@ -0,0 +1,40 @@
+import desktop1 from "../assets/images/desktop-image-hero-1.jpg"
+import desktop2 from "../assets/images/desktop-image-hero-2.jpg";
+import desktop3 from "../assets/images/desktop-image-hero-3.jpg";
+import mobile1 from "../assets/images/mobile-image-hero-1.jpg";
+import mobile2 from "../assets/images/mobile-image-hero-2.jpg";
+import mobile3 from "../assets/images/mobile-image-hero-3.jpg";
+
+export const header = "room";
+
+export const nav = ["home", "shop", "about", "contact"];
+
+export const about = {
+  title: "ABOUT OUR FURNITURE",
+  content:
+    "Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and whatinspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.",
+};
+
+export const posts = [
+  {
+    title: "Discover innovative ways to decorate",
+    content:
+      "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    desktopImage: desktop1,
+    mobileImage: mobile1,
+  },
+  {
+    title: "We are available all across the globe",
+    content:
+      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    desktopImage: desktop2,
+    mobileImage: mobile2,
+  },
+  {
+    title: "Manufactured with the best materials",
+    content:
+      "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    desktopImage: desktop3,
+    mobileImage: mobile3,
+  },
+];
